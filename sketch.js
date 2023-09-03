5bd6cd00b0eb09e0de9785e4c0fa7ae2a3b72969
camfung <78012867+camfung@users.noreply.github.com>
2023-09-03 13:40:38 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,43 +1,108 @@
-class Player {
-  constructor(x, y, fov) {
-      this.pos = createVector(x, y);
-      this.rays = []; 
-      for (let i = 0; i < fov; i+=2) {
-        this.rays.push( new Ray( this.pos, radians(i) ) );
-      }
-  }
+let boundaries = []; 
+let d = 100; 
+let v = 10; 
+let chaser;
+let player;
+let updatePlayer = false
+let width;
+let height;
+let pos; 
+let speed = 3;
+let playerSpeed = 4;
+
+
+function setupWalls() {
+  // make walls surrounding the canvas
+  boundaries.push(new Boundary(0, 0, width, 0));
+  boundaries.push(new Boundary(width, 0, width, height));
+  boundaries.push(new Boundary(width, height, 0, height));
+  boundaries.push(new Boundary(0, height, 0, 0));
+
+  // make a box 
+  // boundaries.push(new Boundary(100, 100, 100, 200));
+  // boundaries.push(new Boundary(100, 200, 200, 200));
+  // boundaries.push(new Boundary(200, 200, 200, 100));
+  // boundaries.push(new Boundary(200, 100, 100, 100));
   
-  update(x, y) {
-      circle(x,y, 10)
-      this.pos.x = x; 
-      this.pos.y = y; 
+}
+
+function setup() {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  pos = new createVector(10, 10)
+  chaser = new Player(pos.x, pos.y, 0);
+  player = new Player(50, 50, 0)
+  setupWalls();
+  createCanvas(width, height);
+
+}
+
+function getNormVec(p1, p2) {
+  //p1 is the origin 
+  // p2 is the dest
+
+  const vx = p2.x - p1.x
+  const vy = p2.y - p1.y
+
+  dist = sqrt((vx*vx) + (vy*vy));
+
+  if (dist < 20) {
+    return createVector(0,0);
   }
 
-  updateRays(boundaries) {
-      for (let ray of this.rays) {
-          let record = Infinity;
-          let closest = null;
-          for (boundary of boundaries) {
-            const pt = ray.cast(boundary); 
-            if (pt) {
-              const d = p5.Vector.dist(this.pos, pt);
-              if (d < record) {
-                record = d;
-                closest = pt;
-              }
-            } 
-          }
-          if (closest) {
-              stroke(ray.color, 100);
-              ellipse()
-              line(this.pos.x, this.pos.y, closest.x, closest.y);
-            }
-        }
-  }
+  const normVec =  createVector(vx / dist, vy/dist);
+  return normVec;
+}
 
-  move(dx, dy) {
-      // Update the player's position
-      this.pos.x += dx;
-      this.pos.y += dy;
-    }
+function draw() {
+  background(0);
+  handleWASDMovement()
+  let mouse = player.pos
+  const normVec = getNormVec(pos, mouse);
+
+
+  pos = pos.add(normVec.mult(speed))
+  chaser.update(pos.x, pos.y)
+  player.update(player.pos.x, player.pos.y)
+  console.log(player.pos)
+
+  // for (boundary of boundaries) {
+  //   boundary.update(d);
+  //   boundary.show();
+  // }
+  stroke(255)
+  line(chaser.pos.x, chaser.pos.y, player.pos.x, player.pos.y)
+  chaser.updateRays(boundaries);
+// frameRate(1)
+
+}
+
+function handleArrowKeyMovement() {
+  if (keyIsDown(LEFT_ARROW)) {
+    player.move(-playerSpeed, 0);
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    player.move(playerSpeed, 0);
+  }
+  if (keyIsDown(UP_ARROW)) {
+    player.move(0, -playerSpeed);
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    player.move(0, playerSpeed);
+  }
+}
+
+function handleWASDMovement() {
+  if (keyIsDown(65)) { // 'A' key for left
+    player.move(-playerSpeed, 0);
+  }
+  if (keyIsDown(68)) { // 'D' key for right
+    player.move(playerSpeed, 0);
+  }
+  if (keyIsDown(87)) { // 'W' key for up
+    player.move(0, -playerSpeed);
+  }
+  if (keyIsDown(83)) { // 'S' key for down
+    player.move(0, playerSpeed);
+  }
 }
